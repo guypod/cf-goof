76334329bcf3012936b637f25ebdd00f169c848a
Guy Podjarny <guypod@gmail.com>
2017-10-11 10:03:07 +0200

    added cfenv

--- db.js
+++ db.js
@@ -1,15 +1,20 @@
 var mongoose = require('mongoose');
+var cfenv = require("cfenv");
 var Schema   = mongoose.Schema;
 
 var Todo = new Schema({
   content    : Buffer,
   updated_at : Date
 });
-
 mongoose.model('Todo', Todo);
+
+var appEnv = cfenv.getAppEnv()
+var mongoEnvUri = appEnv.getServiceURL('goof-mongo');
+console.log("MongoEnvUrl:" + mongoEnvUri)
 var mongoUri = 'mongodb://localhost/express-todo';
-if (process.env.MONGOLAB_URI) {
-	mongoUri = process.env.MONGOLAB_URI;
+if (mongoEnvUri) {
+	mongoUri = mongoEnvUri;
 }
+console.log("Mongo URI:" + mongoUri);
 
 mongoose.connect(mongoUri);
